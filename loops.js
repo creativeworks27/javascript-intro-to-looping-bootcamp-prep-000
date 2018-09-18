b227b20458db53afef722758ca25dcfa66e71b89
Roberto Romero <creativeworks27@icloud.com>
2018-09-18 18:35:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,5 +26,6 @@ function doWhileLoop(array){
   do {
   array.pop();incrementVariable(); 
   }
-  while(array.length<0 && incrementVariable());
+  while(array.length > 0 && incrementVariable());
+  return array;
 }
