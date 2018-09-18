f1c963d9f362afdcd63a68b160a19d21ecb894cf
Roberto Romero <creativeworks27@icloud.com>
2018-09-18 17:50:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,11 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-  if (i === 1)
-  array.push()
-  console.log("I am " + ${i} + "strage loops.");  
+  if (i === 1){
+  array.push("I am 1 strange loop.")
+  }
+  else(){
+  console.log("I am " + ${i} + "strage loops."); 
+  }
   }
   
 }
